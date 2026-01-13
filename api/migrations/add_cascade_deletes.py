c8fd817c1a8b3c5c73ffc4b53664fd28d9108c99
ouckah <aidan.ouckama@gmail.com>
2026-01-13 05:16:24 -0500

    api - migration fixed abg col ref

--- api/migrations/add_cascade_deletes.py
+++ api/migrations/add_cascade_deletes.py
@@ -115,7 +115,7 @@ def run_migration():
         try:
             # Check if constraint already has CASCADE
             constraint_result = connection.execute(text("""
-                SELECT constraint_name, delete_rule
+                SELECT tc.constraint_name, rc.delete_rule
                 FROM information_schema.table_constraints tc
                 JOIN information_schema.referential_constraints rc 
                     ON tc.constraint_name = rc.constraint_name
